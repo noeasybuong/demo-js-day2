fbc09dcc8c81880d699df111fab0b8d6f8ade826
noeasybuong <tangduchoang34@gmail.com>
2023-04-25 10:13:26 +0700

    commit

--- main.js
+++ main.js
@@ -1,77 +1,65 @@
 //bai 1
-var myName = "Tăng Đức Hoàng";
+
 
 function myString(myName) {
 
 
-    console.log(myName.slice(0, 10) + "....");
+    return myName.slice(0, 10) + "....";
 }
-myString(myName);
+myString("Tăng Đức Hoàng");
 
-
+console.log(myString("Tăng Đức Hoàng"));
 //bai 2
 
 function myNameCompany(nameCompany) {
     var comPany = nameCompany.toLowerCase();
 
-    console.log(comPany.charAt(0).toUpperCase() + comPany.slice(1));
+    return comPany.charAt(0).toUpperCase() + comPany.slice(1);
 }
 
 myNameCompany("morSoftware");
 
-
+console.log(myNameCompany("morSoftware"));
 
 
 
 //bài 3
 
 function myAarrayUp(myArray) {
-    var myUp = myArray.filter(num => num % 2 == 0).sort((a, b) => b - a)
-    console.log(myUp)
+    var myUp = myArray.filter(num => num % 2 == 0).sort((a, b) => b - a);
+    return myUp;
 }
-myAarrayUp(myArray = [18, 6, , 2, 9, 10]);
+myAarrayUp([18, 6, , 2, 9, 10]);
 
 
+console.log(myAarrayUp(myArray = [18, 6, , 2, 9, 10]));
 
 //bài 4
 function conFimUser(User) {
 
 
-    //username
-    if (User.userName.length >= 6 && User.userName.length <= 32) {
-        console.log("Pass");
-    }
+    if (User.userName.length < 6 && User.userName.length > 32) return "Fail";
 
-    else {
-        console.log("Fail");
-    }
-
-    //password
-
-    if (User.passWord.length >= 6 && User.passWord.length <= 32) {
-        console.log("Pass");
-    }
-
-    else {
-        console.log("Fail");
-    }
+    if (User.passWord.length < 6 && User.passWord.length > 32) return "Fail";
+    if (User.passWord !== User.conFirm) return "Fail";
 
 
 
-    //confim
-    if (User.conFim.length >= 6 && User.conFim.length <= 32 && User.conFim === User.passWord) {
-        console.log(" Pass");
-    }
 
-    else {
-        console.log("Fail");
-    }
+    return "Pass";
 
 }
 
 conFimUser(
-    User = {
+    {
         userName: "username ",
         passWord: "password",
-        conFim: "password",
+        conFirm: "password",
     })
+
+console.log(conFimUser(
+    {
+        userName: "username ",
+        passWord: "password",
+        conFirm: "password",
+    }));
